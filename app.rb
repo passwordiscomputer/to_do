ec469965ea1b310abf7f876310ea3115e91bb69c
Andrew Peterson and Chase Bowden <chase@>
2018-02-20 10:14:46 -0800

    add

---
+++ app.rb
@@ -0,0 +1,30 @@
+require('sinatra')
+require('sinatra/reloader')
+require('./lib/task')
+require('./lib/list')
+also_reload('lib/**/*.rb')
+require("pg")
+
+DB = PG.connect({:dbname => "to_do"})
+
+
+get("/") do
+  erb(:index)
+end
+
+get("/lists/new") do
+  erb(:list_form)
+end
+
+post("/lists") do
+  name = params.fetch("name")
+  list = List.new({:name => name, :id => nil})
+  list.save()
+  erb(:list_success)
+ end
+
+
+ get('/lists') do
+   @lists = List.all()
+   erb(:lists)
+ end
